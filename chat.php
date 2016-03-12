8b9429e63c6e1ff5c4c6bbe2c9d9d958fb97353f
JosephYqb <yqb1039961917@gmail.com>
2016-03-12 23:19:50 +0800

    前端页面

--- chat.php
+++ chat.php
@@ -1,19 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/3/11
- * Time: 17:26
- */
 require_once './Workerman/Autoloader.php';
 use Workerman\Worker;
 
 
-$chat_work = new Worker('websocket://0.0.0.0:6666');
-$chat_work->onMessage = function($connect,$data){
-  global $chat_work;
-    foreach($chat_work->connections as $connection){
-        $connection->send($data);
-    }
+$worker = new Worker('websocket://0.0.0.0:5555');
+$worker->onConnect =function($connect){
+    $connect->ip = explode(' ',$_SERVER['SSH_CONNECTION'])[0];
 };
-$chat_work::runAll();
+$worker->onMessage =function($connect,$data){
+	  global $worker;
+    foreach($worker->connections as $connection){
+        $connection->send($connect->ip .':'. htmlspecialchars($data));
+    }
+
+};
+Worker::runAll();
